98eb8563bf5f730016431e6290233a66a44c7776
torla <munford.ns@gmail.com>
2023-01-30 11:23:30 +0100

    pandas time and join

---
+++ pandas/examples/03_time_and_date.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import datetime
+
+with open('pandas/data/d202.csv', 'r') as fp:
+    df = pd.read_csv(fp)
+# create a datetime column using apply to multiple column
+df["time"] = df[['DATE', 'END TIME']].apply(
+    lambda x: datetime.datetime.strptime(f"{x.DATE} {x['END TIME']}", '%m/%d/%Y %H:%M'), axis=1)
+df.set_index('time', inplace=True)
+
+# is often useful to have data at clear fixed interval, this can be done using the .resample method
+print(df['USAGE'].resample('1H').sum().head())  # resample to hour interval and sum
+
+# one can also upsample using a filling method
+print(df['USAGE'].groupby(by='time').sum().resample('30S').ffill().head())  # groupby to remove duplicated indexes
+
+# another widely used method is windowing
+print(df['USAGE'].resample('1H').sum().rolling(
+    window=4).mean().head())  # computing the hour average with a rolling window of 4 hour
--- pandas/examples/05_reshaping_and trasforming.py
+++ pandas/examples/05_reshaping_and trasforming.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 url = "https://storage.googleapis.com/academy_datasets/exercises/california-house-prices/california_housing_test.csv"
@@ -21,5 +22,14 @@ print(df['total_rooms'].drop_duplicates().count() == len(df['total_rooms'].uniqu
 
 # pandas features also a sql like groupby
 print("group")
-print(df.groupby("total_rooms").mean()) # group by colums and aggregate by sum
-
+print(df.groupby("total_rooms").mean())  # group by column and aggregate by average
+print(df.groupby("total_rooms").sum())  # group by column and aggregate by sum
+print(df.groupby("total_rooms").agg({"median_income": lambda x: np.min(x)}))  # group by column custom agg
+# also a sql like join (default left join)
+df1 = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
+                    'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})
+df2 = pd.DataFrame({'key': ['K0', 'K1', 'K2'],
+                    'B': ['B0', 'B1', 'B2']})
+print(df1.join(df2.set_index('key'), on='key', how='inner'))
+# or the merge methods, similar to join but not working on indexes (default inner)
+print(df1.merge(df2, left_on='key', right_on='key', how='left'))
