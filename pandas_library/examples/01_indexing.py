7f00789ce7bb4780824d06f8e51450086a32a838
torla <munford.ns@gmail.com>
2023-02-10 10:32:49 +0100

    pandas index types

--- pandas_library/examples/01_indexing.py
+++ pandas_library/examples/01_indexing.py
@@ -1,3 +1,5 @@
+import datetime
+
 import pandas as pd
 
 with open('pandas/data/D202.csv', 'r') as fp:
@@ -31,7 +33,13 @@ print(df.loc[5].at['COST'])  # also in a series
 print(df.iat[1, 2])
 print(df.loc[5].iat[2])  # also in a series
 
-# todo index types
+# index need to be unique but they can be of different dtype
+pd.RangeIndex(start=1, stop=10, step=2)  # Range of int
+pd.CategoricalIndex(categories=["a", "b", "c", "d"], dtype='category', ordered=True)  # list of label ,can be ordered
+pd.DatetimeIndex(data=[datetime.datetime(year=2021, month=1, day=1), datetime.datetime(year=2022, month=1, day=1),
+                       datetime.datetime(year=2023, month=1, day=1)],
+                 freq="infer", closed="left", )  # datetime
+pd.PeriodIndex(year=[2020, 2021, 2022], month=[1, 1, 1], freq="w")  # period first week of 2020,2021,2022
 
 # dataframe con be filtered by using boolean indexing
 # a comparison exp return a series of boolean
